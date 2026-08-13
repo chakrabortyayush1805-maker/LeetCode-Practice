de550b9fb8e3b43356f47241ceb8adc4865db9a5
chakrabortyayush1805-maker <chakrabortyayush1805@gmail.com>
2026-08-13 18:39:39 +0530

    Time: 0 ms (100%), Space: 44.6 MB (65.48%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder current, int openCount, int closeCount, int max) {
+        if (current.length() == max * 2) {
+            result.add(current.toString());
+            return;
+        }
+
+        if (openCount < max) {
+            current.append('(');
+            backtrack(result, current, openCount + 1, closeCount, max);
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        if (closeCount < openCount) {
+            current.append(')');
+            backtrack(result, current, openCount, closeCount + 1, max);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+}
