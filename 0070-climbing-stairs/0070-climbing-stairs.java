42b16edcda7c131895c6ace8b65e8732f171aa77
chakrabortyayush1805-maker <chakrabortyayush1805@gmail.com>
2026-08-25 18:28:13 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,7 +5,7 @@ class Solution {
         }
 
         int prev2 = 1;
-        int prev1 = 2; 
+        int prev1 = 2;
 
         for (int i = 3; i <= n; i++) {
             int current = prev1 + prev2;
