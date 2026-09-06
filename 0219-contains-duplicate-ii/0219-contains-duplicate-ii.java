69392daccac2a7d4c15839177923226cb2e1ea61
chakrabortyayush1805-maker <chakrabortyayush1805@gmail.com>
2026-09-06 20:02:25 +0530

    Time: 25 ms (85.18%), Space: 108.4 MB (54.54%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (k <= 0 || nums == null || nums.length < 2) {
+            return false;
+        }
+
+        Set<Integer> window = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!window.add(nums[i])) {
+                return true;
+            }
+
+            if (window.size() > k) {
+                window.remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
